eb2165a7dfb89c9978dad1490ea64f08cafc1f73
Bela <belahamid@gmail.com>
2021-02-24 17:25:31 +0000

    GET request for whiskies by year is successful

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,10 @@ public class WhiskyController {
         return new ResponseEntity<>(whiskyRepository.findAll(), HttpStatus.OK);
     }
 
+    @GetMapping(value="whiskies/{year}")
+    public ResponseEntity<List<Whisky>> getWhiskiesByYear(@PathVariable int year){
+        return new ResponseEntity<>(whiskyRepository.findAllWhiskiesByYear(year), HttpStatus.OK);
+    }
 
 
 }
